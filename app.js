05224b50f33bf8426c400a83a21adde256fe1eea
Ilija Trajkoski <ile2807@gmail.com>
2021-05-31 14:16:31 +0200

    Multiple collections support added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const args = parseArgs(process.argv.slice(2));
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const { exit } = require('process');
 const { help, line, version } = require('./lib/helper');
 const { validateParametersSourceFile, validateParametersSourceFolder } = require('./lib/validation');
 const outputFileName = args.o;
@@ -10,7 +11,7 @@ const collectionsFolder = args.f;
 const jackal = require("jackal-postman");
 const command = process.argv.slice(2)[0];
 const sourceFileName = args.s;
-
+const collectionNamesList = args.c;
 clear();
 
 if (args.v || args.version) {
@@ -30,22 +31,23 @@ if (args.h || args.help) {
 if (command === undefined) help();
 
 if (command.startsWith("t") || command.startsWith("clr") || command.startsWith("amcv")) {
-    validateParametersSourceFile(sourceFileName, outputFileName);
+    if (!validateParametersSourceFile(sourceFileName, outputFileName)) exit();
 } else {
-    validateParametersSourceFolder(collectionsFolder, outputFileName);
+    if (!validateParametersSourceFolder(collectionsFolder, outputFileName, collectionNamesList)) exit();
 }
 console.log('Source collections folder > ' + chalk.cyan(collectionsFolder));
-console.log('Start collection > ' + chalk.cyan(sourceFileName));
+console.log('Source collections > ' + chalk.cyan(collectionNamesList));
+console.log('Start file > ' + chalk.cyan(sourceFileName));
 console.log('Target collection > ' + chalk.cyan(outputFileName));
 console.log('Command > ' + chalk.cyan(command))
 line();
-jackal.run(command, sourceFileName, collectionsFolder, outputFileName).then(executionResult => {
+jackal.run(command, sourceFileName, collectionsFolder, outputFileName, collectionNamesList).then(executionResult => {
     if (executionResult instanceof Set) {
         executionResult.forEach(e => console.log(chalk.yellow(e)));
     } else {
         console.log("Execution: " + chalk.yellow(executionResult));
     }
-});
+}).catch(err=>console.log(err));
 
 
 
